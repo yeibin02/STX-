e97a4234e817a6c55944811f74890dbd52789ab7
yeibin02 <yeibin02@gmail.com>
2023-06-28 17:37:45 +0900

    자바스크립트 수정

--- js/script.js
+++ js/script.js
@@ -6,6 +6,22 @@ window.onload = function () {
       section.scrollIntoView({ behavior: "smooth" });
     }
   }
+  //비주얼 이동 버튼 스크롤 기능
+  const button = document.querySelector(".visual-bt");
+  //버튼을 클릭했을 때 스크롤 이동하는 함수를 실행합니다.
+  button.addEventListener("click", function () {
+    scrollToSection("#business");
+  });
+  //scrollToSection() 함수를 정의합니다.
+  function scrollToSection(sectionId) {
+    //sectionId로 지정된 요소를 찾아서 section 변수에 저장합니다.
+    const section = document.querySelector(sectionId);
+    //section이 존재한다면 스크롤 이동을 수행합니다.
+    if (section) {
+      //부드러운 스크롤 이동을 위해 scrollIntoView 매소드를 호출합니다.
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  }
   // top 버튼 스크롤 기능
   const topBtn = document.getElementById("top-btn");
   topBtn.addEventListener("click", function (event) {
@@ -30,7 +46,14 @@ window.onload = function () {
     if (scTop > 0) {
       topBtnImg.classList.add("up");
     } else {
-        topBtnImg.classList.remove("up");
+      topBtnImg.classList.remove("up");
     }
+  });
+  // 안내창
+  let body = document.querySelector("body");
+  let modal = document.querySelector(".modal-wrap");
+  modal.addEventListener("click", function () {
+    modal.style.display = "none";
+    fadeOut(modal);
   });
 };
